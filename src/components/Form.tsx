20a38899d820a7ec96d0919d39a2d769736dbbe4
Pochinsky <tomas.guttman@sansano.usm.cl>
2024-11-10 21:21:12 -0300

    ✨ form finished

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { useState, ChangeEvent, FormEvent, Dispatch } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { categories } from "../data/categories";
 import type { Activity } from "../types";
 import { ActivityActions } from "../reducers/activity-reducer";
@@ -7,12 +8,15 @@ type FormProps = {
   dispatch: Dispatch<ActivityActions>;
 };
 
+const initialState: Activity = {
+  id: uuidv4(),
+  category: 1,
+  name: "",
+  calories: 0,
+};
+
 export default function Form({ dispatch }: FormProps) {
-  const [activity, setActivity] = useState<Activity>({
-    category: 1,
-    name: "",
-    calories: 0,
-  });
+  const [activity, setActivity] = useState<Activity>(initialState);
 
   const isValidActivity = () =>
     activity.name.trim() !== "" && activity.calories > 0;
@@ -30,6 +34,7 @@ export default function Form({ dispatch }: FormProps) {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     dispatch({ type: "save-activity", payload: { newActivity: activity } });
+    setActivity({ ...initialState, id: uuidv4() });
   };
 
   return (
